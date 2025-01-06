47043cb80270dd418752b6bfd497b76f7a09de9b
czdtech <chongzhidatech@gmail.com>
2025-01-07 02:32:43 +0800

    Integrate Alpine.js for interactive FAQ component and update configurations: added @astrojs/alpinejs dependency, modified FAQ section to utilize Alpine.js for toggle functionality, and updated tsconfig.json to include additional file types. This enhances user experience and improves code organization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fiddlebops.com',
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+  site: "https://fiddlebops.com",
+  integrations: [tailwind(), sitemap(), alpinejs()],
 });
